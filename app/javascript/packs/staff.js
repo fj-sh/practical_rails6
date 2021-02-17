5e5d76bb16ba9a7b60536bbb1c3bb1042034c8ab
fj-sh <menspocky322@gmail.com>
2021-02-17 16:58:06 +0900

    プロジェクトを初期化した。

---
+++ app/javascript/packs/staff.js
@@ -0,0 +1,8 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "../staff/customer_form.js";
+import "../staff/entries_form.js";
+import "../staff/messages.js";
